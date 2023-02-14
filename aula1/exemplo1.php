c9ef786d9d9b607589f6edc22265d99f983642e9
Breno de Souza Vieira <48176406856@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-02-14 08:44:23 -0300

    first commit

---
+++ aula1/exemplo1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php 
+        for($c=1; $c <= 50; $c++) {
+    ?>
+
+    <img src="https://i.pinimg.com/736x/ff/b5/f1/ffb5f10112fbf6a03bcddd6619b63834.jpg" alt="Paisagem" width="10%">
+
+    <?php } ?>
+
+</body>
+</html>
